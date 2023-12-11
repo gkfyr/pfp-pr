870b79329071b88b3e88f2337ace37a33b8e4948
gkfyr <harock050110@naver.com>
2023-12-11 10:08:16 +0900

    first commit

---
+++ frontend/src/pages/mint.jsx
@@ -0,0 +1,51 @@
+import { useContext, useState } from "react";
+
+import axios from "axios";
+import { AppContext } from "../App";
+
+const account = "0x1AF50EB750aea2CC095A06dF301737f4057fB100";
+
+const Mint = () => {
+  const { contract } = useContext(AppContext);
+
+  const [metadata, setMetadata] = useState();
+
+  const onClickMint = async () => {
+    try {
+      if (!account || !contract) return;
+
+      await contract.methods.mintNFT().send({
+        from: account,
+      });
+
+      const balance = await contract.methods.balanceOf(account).call();
+
+      const newTokenId = await contract.methods
+        .tokenOfOwnerByIndex(account, Number(balance) - 1)
+        .call();
+
+      const metadataURI = await contract.methods
+        .tokenURI(Number(newTokenId))
+        .call();
+
+      const response = await axios.get(metadataURI);
+
+      setMetadata(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="bg-red-100 max-w-screen-md mx-auto min-h-screen flex flex-col justify-center items-center">
+      {metadata && (
+        <div>
+          <img src={metadata.image} alt={metadata.name} />
+        </div>
+      )}
+      <button onClick={onClickMint}>Mint</button>
+    </div>
+  );
+};
+
+export default Mint;
